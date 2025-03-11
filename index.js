9d500d1dd6a3a9621c66646c746dd1e3047b94db
Arlene Garcia <146496596+arlenecse@users.noreply.github.com>
2025-03-11 10:26:40 -0700

    Update theme button index.js

--- index.js
+++ index.js
@@ -5,16 +5,18 @@
 
 
 // Step 1: Select the theme button
-
+let themeButton = document.getElementById("theme-button");
 
 // Step 2: Write the callback function
 const toggleDarkMode = () => {
     // Write your code here
+    document.body.classList.toggle("dark-mode");
     // This section will run whenever the button is clicked
 }
 
 
 // Step 3: Register a 'click' event listener for the theme button,
+themeButton.addEventListener("click", toggleDarkMode);
 //             and tell it to use toggleDarkMode as its callback function
 
 
