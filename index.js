3217490d863d7117be2a38ed99aa283b532763c4
Shourya-Kulkarni <sdkulkarni@ucsd.edu>
2025-03-13 13:52:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,24 @@ const toggleDarkMode = () => {
 themeButton.addEventListener("click", toggleDarkMode);
 //             and tell it to use toggleDarkMode as its callback function
 
-
-
-
 /*** Form Handling [PLACEHOLDER] ***/
+const form = document.getElementById("connect-form");
+const participantsDiv = document.querySelector(".connect-participants");
+const countElement = document.getElementById("connect-count");
+
+form.addEventListener("submit", (event) => {event.preventDefault();});
+
+let nameInput = document.getElementById("name").value;
+let stateInput = document.getElementById("state").value;
+
+if (nameInput && stateInput) {
+    let newParticipant = document.createElement("p");
+    newParticipant.textContent = `⭐ ${nameInput} from ${stateInput} is joining the crew.`;
+    participantsDiv.appendChild(newParticipant);
+    count = count +1;
+    countElement.textContent =  "⭐" + count + " people have RSVP'd to this event!"
+;
+
 /*** Form Validation [PLACEHOLDER] ***/
 /*** Animations [PLACEHOLDER] ***/
 /*** Success Modal [PLACEHOLDER] ***/
